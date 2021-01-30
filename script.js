13dabf4481f0e7cd6497ee6d2c7720a4da474263
kavita-gohel <kavitagohel.rao@gmail.com>
2021-01-30 14:08:08 +0530

    Mockaroo ajax

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+    function loadDoc(url){
+     var xhttp;
+     xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange=function(){
+         if(this.readyState==4 && this.status==200){
+             document.getElementById("myID").innerHTML=xhttp.responseText;
+        }
+     };
+     xhttp.open("POST",url,true);
+     xhttp.send();
+     }
+     function myFunction(xhttp){
+     document.getElementById("myID").innerHTML=xhttp.responseText;
+ }
+
+
 function funx(url,fun){
     var xhttp;
     xhttp=new XMLHttpRequest();
